8abc6882cccf6aeb7418822e0645ba6fbdfe0e26
Rachael Birky <rbirky1@jhu.edu>
2017-04-09 05:47:06 -0400

    Added bg image, get_names support, and search again.

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var $searchBtn = $("#surname-search-btn");
 // Get spinner
 var $spinner = $("#loading-spinner");
 var $chartContainer = $("#chart-container");
+var $searchAgain = $("#search-again-btn");
 // Map indices to field names T_T
 var mapping = [
     "White",
@@ -18,13 +19,18 @@ var mapping = [
 var ip = '127.0.0.1';
 var port = 5000;
 var stats = 'get_stats';
+var names = 'get_names';
 
 $(function() {
 
     /* Initialization */
     // Get list of names in database for field autocomplete
-    $.get("data/example_collection.json", function(data){
-        $surnameInput.typeahead({ source: data });
+    var names_url = 'http://'+ip+":"+port+"/"+names;
+//    var names_url = 'data/example_collection.json';
+    $.get(names_url, function(data){
+        console.log(data.names);
+        $surnameInput.typeahead({ source: data.names });
+        $surnameInput.attr("disabled",false).focus();
     });
 
     /* Event handlers */
@@ -37,26 +43,22 @@ $(function() {
     // On surname-search-btn click
     $searchBtn.click(function(e) {
         search_for_surname($surnameInput.val());
-    })
-	$surnameInput.keyup(function(e){
+    });
+	$surnameInput.keyup(function(e) {
 		if ($surnameInput.val() === ""){
 			$searchBtn.addClass("disabled");
 		} else {
 			$searchBtn.removeClass("disabled");
 		}
-	})
-
+	});
+	$searchAgain.click(function(e) {
+	    $('html, body').stop().animate({
+            scrollTop: $("#banner").offset().top
+        }, 1000);
+	});
 });
 
 function search_for_surname(surname) {
-	
-	$("#surname-label").text(surname);
-	
-
-    // Scroll to results
-    $('html, body').stop().animate({
-        scrollTop: $("#graphs").offset().top
-    }, 1000);
 
     // Fade in spinner
     //$spinner.fadeIn(2000);
@@ -65,10 +67,17 @@ function search_for_surname(surname) {
     var req = $surnameInput.val();
 
     var stats_url = 'http://'+ip+":"+port+"/"+stats;
-//    $.get(stats_url, function(data){
     $.post( stats_url, req, function(data) {
 		// Remove or fade out spinner
         //$spinner.fadeOut(1000);
+
+        $("#graphs").fadeIn();
+        $("#surname-label").text(surname);
+
+        // Scroll to results
+        $('html, body').stop().animate({
+            scrollTop: $("#graphs").offset().top
+        }, 1000);
 		
 		var json = JSON.parse(data.census);
 		var row = json[0];
@@ -78,6 +87,7 @@ function search_for_surname(surname) {
 			$("#surname-no-data").text(surname);
 			$chartContainer.fadeOut(100);
 			$("#no-data").fadeIn();
+
 		} else {
 		
 			$("#no-data").fadeOut();
@@ -116,11 +126,7 @@ function search_for_surname(surname) {
 		
     })
     .fail(function() {
-        console.log( "error" );
-    })
-    .always(function() {
-        // Remove or fade out spinner
-        $spinner.fadeOut(1000);
+        $('#error-modal').modal('show');
     });
 
 };
